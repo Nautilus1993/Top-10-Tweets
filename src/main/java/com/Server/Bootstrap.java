3bbdc72048e45beec370a59364199c3c56c0e8b9
Yuhang Wang <2271275281@qq.com>
2017-04-11 17:49:12 -0400

    deploy to heroku

--- src/main/java/com/Server/Bootstrap.java
+++ src/main/java/com/Server/Bootstrap.java
@@ -15,8 +15,16 @@ public class Bootstrap {
     public static final int TOP_K = 10;
     public static void main(String[] args) throws Exception {
         setIpAddress(IP_ADDRESS);
-        setPort(PORT);
+        setPort(getHerokuAssignedPort());
         staticFileLocation("/public");
         new RankerController(new RankerService());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
